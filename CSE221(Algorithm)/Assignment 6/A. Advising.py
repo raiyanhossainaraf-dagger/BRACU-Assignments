2b3ea1186973beb5327fd99f3f52b500fe4ed49e
Raiyan Hossain Araf <raiyanhossainaraf@gmail.com>
2025-12-10 19:33:35 +0600

    updated
    Refactor the advising algorithm to store results in a list and print them at once. Ensure proper handling of the case where not all nodes are processed.

--- CSE221(Algorithm)/Assignment 6/A. Advising.py
+++ CSE221(Algorithm)/Assignment 6/A. Advising.py
@@ -8,7 +8,7 @@ N,M=map(int,input().strip().split())
 adjList=[[] for _ in range(N + 1)]
 indeg=[0]*(N+1)
 q=queue.Queue()
-
+result=[]
 
 def addEdgetoMatrix(source, destination):
 
@@ -29,22 +29,26 @@ for  i in range(1,len(indeg)):
            q.put(i)
 
 
-if q.empty()==True:
 
-    print(-1)
+while  not q.empty():
+
+    i=q.get()
+    result.append(i)
+    
+    for j in adjList[i]:
+
+        indeg[j]-=1
+
+        if indeg[j]==0:
+
+            q.put(j)
+
+        
+        
+if len(result)==N:
+
+    print(*result)
 
 else:
-    while  not q.empty():
-
-        i=q.get()
-        print(i)
-        
-        for j in adjList[i]:
-
-            indeg[j]-=1
-
-            if indeg[j]==0:
-
-                q.put(j)
-        
-        
+    
+      print(-1)
